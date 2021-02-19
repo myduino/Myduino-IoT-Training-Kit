df927c0b49c985d8df89132c3dae4dc83d67211b
Amrul Ariffi bin Khalid <amrulariffi@gmail.com>
2021-02-19 16:02:09 +0800

    Change already

---
+++ Exercise_02/Running_LED_Function/Running_LED_Function.ino
@@ -0,0 +1,18 @@
+void setup() {
+    pinMode (13,OUTPUT);
+}
+
+void loop() {
+
+    blinky();
+
+}
+
+void blinky() {
+
+    digitalWrite (13,HIGH);
+    delay (1000);
+    digitalWrite (13, LOW);
+    delay (1000);
+
+}
