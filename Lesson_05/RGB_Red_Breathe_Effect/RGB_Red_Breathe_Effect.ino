de3a7a7a41433861c166197545baca5ede0a2931
ariffinzulkifli <ariffinastute@live.com>
2022-10-31 20:01:32 +0800

    update sketch

--- Lesson_05/RGB_Red_Breathe_Effect/RGB_Red_Breathe_Effect.ino
+++ Lesson_05/RGB_Red_Breathe_Effect/RGB_Red_Breathe_Effect.ino
@@ -2,17 +2,16 @@ int red = 9;
 
 void setup() {
   pinMode (red, OUTPUT);
-
 }
 
 void loop() {
-  for (int i=0;i<40; i++){
-    analogWrite (red,i);
-    delay (50);
-    }
+  for (int brightness = 0; brightness < 40; brightness++){
+    analogWrite(red, brightness);
+    delay(50);
+  }
 
-  for (int i=40     ;i>0; i--){
-    analogWrite (red,i);
-    delay (50);
-    }
+  for (int brightness = 40; brightness > 0; brightness--){
+    analogWrite(red, brightness);
+    delay(50);
+  }
 }
--- Lesson_05/RGB_Red_Brightness/RGB_Red_Brightness.ino
+++ Lesson_05/RGB_Red_Brightness/RGB_Red_Brightness.ino
@@ -1,24 +1,23 @@
 int red = 9;
 
 void setup() {
-  pinMode (red, OUTPUT);
-
+  pinMode(red, OUTPUT);
 }
 
 void loop() {
-  analogWrite (red,0);
-  delay (1000);
+  analogWrite(red,0);
+  delay(1000);
 
-  analogWrite (red,10);
-  delay (1000);
+  analogWrite(red,10);
+  delay(1000);
 
-  analogWrite (red,20);
-  delay (1000);
+  analogWrite(red,20);
+  delay(1000);
 
-  analogWrite (red,30);
-  delay (1000);
+  analogWrite(red,30);
+  delay(1000);
 
-  analogWrite (red,40);
-  delay (1000);
+  analogWrite(red,40);
+  delay(1000);
 
 }
