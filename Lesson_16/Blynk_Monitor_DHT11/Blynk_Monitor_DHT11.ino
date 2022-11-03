a1a3ebab4df4311b89b7e66adf8cd6fa76d15d26
ariffinzulkifli <ariffinastute@live.com>
2022-11-04 01:52:47 +0800

    update sketch

--- Lesson_16/Blynk_Monitor_DHT11/Blynk_Monitor_DHT11.ino
+++ Lesson_16/Blynk_Monitor_DHT11/Blynk_Monitor_DHT11.ino
@@ -1,20 +1,24 @@
-#define BLYNK_PRINT Serial
+#define BLYNK_TEMPLATE_ID "YOUR_TEMPLATE_ID"
+#define BLYNK_DEVICE_NAME "YOUR_DEVICE_NAME"
+#define BLYNK_AUTH_TOKEN  "YOUR_AUTH_TOKEN"
 
+#define BLYNK_PRINT Serial
 
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 #include <SimpleDHT.h>
 
-char auth[] = "m7IgUwXlfiZnAy0ys4RMiBxuk6PFCBA0";
-char ssid[] = "myinvententerprise";
-char pass[] = "04222682";
+char auth[] = BLYNK_AUTH_TOKEN;
+char ssid[] = "YourWiFiName";
+char pass[] = "YourWiFiPassword";
 
 unsigned long previousMillis = 0;
-int pinDHT11 = D5;
+
+int pinDHT11 = D4;
 SimpleDHT11 dht11(pinDHT11);
+
 void setup() {
   Serial.begin(9600);
-
   Blynk.begin(auth, ssid, pass);
 
 }
@@ -22,16 +26,27 @@ void setup() {
 void loop() {
   Blynk.run();
 
-  unsigned long currentMillis = millis();
+  byte temperature = 0;
+  byte humidity = 0;
 
-  if (currentMillis - previousMillis >1500){
+  int err = SimpleDHTErrSuccess;
+  if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+    // Serial.print("Read DHT11 failed, err="); Serial.print(SimpleDHTErrCode(err));
+    // Serial.print(","); Serial.println(SimpleDHTErrDuration(err)); delay(1000);
+    return;
+  }
+
+  Serial.println("=================================");
+  Serial.println("Sample DHT11...");
+  
+  Serial.print("Sample OK: ");
+  Serial.print((int)temperature); Serial.print(" *C, "); 
+  Serial.print((int)humidity); Serial.println(" H");
+
+  if (millis() - previousMillis > 1500){
     previousMillis = millis ();
-    byte temperature = 0;
-    byte humidity = 0;
-    
 
-  dht11.read(&temperature, &humidity, NULL);
-  Blynk.virtualWrite (V1, (int)temperature);
-  Blynk.virtualWrite (V2, (int)humidity);
+    Blynk.virtualWrite (V1, (int)temperature);
+    Blynk.virtualWrite (V2, (int)humidity);
   }
 }
