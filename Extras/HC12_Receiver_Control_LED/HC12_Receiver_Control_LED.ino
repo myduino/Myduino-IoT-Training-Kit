4d0cdab3652f9f2fc9d7bbdf9903183808585a19
ariffinzulkifli <ariffinastute@live.com>
2024-05-06 14:12:36 +0800

    update sketch

--- Extras/HC12_Receiver_Control_LED/HC12_Receiver_Control_LED.ino
+++ Extras/HC12_Receiver_Control_LED/HC12_Receiver_Control_LED.ino
@@ -5,12 +5,24 @@ SoftwareSerial HC12(10, 11);  // HC12 using pins 10 (RX) and 11 (TX)
 void setup() {
   Serial.begin(9600);  // Initialize the Serial Communication with the PC
   HC12.begin(9600);    // Initialize the HC12 Module Serial Communication
+
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
   // Read data from HC12 and send it to the serial monitor
-  while (HC12.available()) {
-    Serial.write(HC12.read());
+  if (HC12.available()) {
+    
+    String data = HC12.readString();
+
+    if (data == "ON") {
+      digitalWrite(13, HIGH);
+    }
+    else if(data == "OFF") {
+      digitalWrite(13, LOW);
+    }
+    
+    Serial.println(data);
   }
 
   // Read data from the serial monitor and send it to the HC12 module
